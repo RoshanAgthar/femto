4818b65acbb7e413240c58945d64c1267b65e055
RoshanAgthar <roshanagthar4@gmail.com>
2025-03-24 12:36:31 +0530

    file craeted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { SampleComponent } from './sample/sample.component'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ SampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hospitalmanagement';
+ 
 }
